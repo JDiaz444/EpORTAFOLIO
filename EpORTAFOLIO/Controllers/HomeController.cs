26b42bc3195d685b179f8acf8302184151a81633
JAaronDV <Astraphobia@NITRO5>
2023-06-01 11:35:05 -0700

    Se agregó el modelo al proyecto

--- EpORTAFOLIO/Controllers/HomeController.cs
+++ EpORTAFOLIO/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EpORTAFOLIO.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,11 +24,48 @@ namespace EpORTAFOLIO.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contacto()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(new Contactooo());
         }
+
+        [HttpPost]
+        public ActionResult Enviar(Contactooo contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Crear un objeto MimeMessage con la información del formulario.
+                    var mensaje = new MimeMessage();
+                    mensaje.From.Add(new MailboxAddress("Mi portafolio", "Portafolio"));
+                    mensaje.To.Add(new MailboxAddress("Aaron Diaz", "jdiaz44@uabc.edu.mx"));
+                    mensaje.Subject = "New Contact from the Form";
+                    mensaje.Body = new TextPart("plain")
+                    {
+                        Text = string.Format("Hello, a new HostSchool wants to get in touch with IAG.\nFull Name: {0}\nEmail: {1}\nPhone: {2}\nMessage: {3}",
+                            contacto.FullName, contacto.Correo, contacto.Telefono, contacto.Mensaje)
+                    };
+
+                    // Enviar el correo electrónico utilizando el servidor SMTP de Gmail.
+                    using (var clienteSmtp = new SmtpClient())
+                    {
+                        clienteSmtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                        clienteSmtp.Authenticate("programacionvisual261@gmail.com", "mrctyjiiugkikxpf");
+                        clienteSmtp.Send(mensaje);
+                        clienteSmtp.Disconnect(true);
+                    }
+
+                    return RedirectToAction("Contacto");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Ocurrió un error al enviar el correo electrónico. Por favor, intenta de nuevo más tarde.");
+                }
+            }
+
+            return View("Contacto", contacto);
+        }
+
     }
 }
